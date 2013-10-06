2302652b0d97e4c7718a3f76c13c4c6add9d27ef
Ivar Clemens <post@ivarclemens.nl>
2013-10-06 12:07:58 +0200

    Whitespace.

--- src/videoroi/VideoROIDatasetController.m
+++ src/videoroi/VideoROIDatasetController.m
@@ -1,119 +1,119 @@
 classdef VideoROIDatasetController < handle
-    methods(Access = public)
-        function obj = VideoROIDatasetController(project, dataset)
-            obj.project = project;
-            obj.dataset = dataset;
-            
-            obj.view = VideoROIDatasetView();
-            obj.view.setResolution(obj.dataset.getScreenResolution());
-            obj.view.setNumberOfTrials(obj.dataset.getNumberOfTrials());
-
-            obj.view.addEventListener('changeTrial', @(src, value) obj.onChangeTrial(src, value));
-            obj.view.addEventListener('changeTime', @(src, value) obj.onChangeTime(src, value));
-            
-            obj.onChangeTrial([], 1);
-        end
+  methods(Access = public)
+    function obj = VideoROIDatasetController(project, dataset)
+      obj.project = project;
+      obj.dataset = dataset;
+      
+      obj.view = VideoROIDatasetView();
+      obj.view.setResolution(obj.dataset.getScreenResolution());
+      obj.view.setNumberOfTrials(obj.dataset.getNumberOfTrials());
+      
+      obj.view.addEventListener('changeTrial', @(src, value) obj.onChangeTrial(src, value));
+      obj.view.addEventListener('changeTime', @(src, value) obj.onChangeTime(src, value));
+      
+      obj.onChangeTrial([], 1);
     end
-
-    properties(Access = private)
-        view = [];
-
-        % Project to load stimuli from
-        project = [];
-        
-        % Dataset to display
-        dataset = [];
-        
-        % Stimulus cache
-        stimCache = cell(0, 2);
-        
-        currentTrial;
+  end
+  
+  properties(Access = private)
+    view = [];
+    
+    % Project to load stimuli from
+    project = [];
+    
+    % Dataset to display
+    dataset = [];
+    
+    % Stimulus cache
+    stimCache = cell(0, 2);
+    
+    currentTrial;
+  end
+  
+  methods(Access = private)
+    
+    %
+    % Load/cache stimulus
+    %
+    function stim = loadStimulus(obj, stimulus)
+      index = find(strcmp({obj.stimCache{:, 1}}, stimulus.name));
+      
+      if ~isempty(index)
+        stim = obj.stimCache{index(1), 2};
+        return;
+      end
+      
+      [~, filename, ~] = fileparts(stimulus.name);
+      stimInfo = obj.project.getInfoForStimulus(filename);
+      
+      if ~isstruct(stimInfo)
+        warning('Invalid stimulus specified');
+        stim = [];
+        return;
+      end;
+      
+      stimFile = fullfile(stimInfo.resourcepath, stimInfo.filename);
+      
+      h = waitbar(0, 'Opening stimulus...');
+      
+      try
+        stim = VideoROIStimulus();
+        stim.openStimulus(stimFile);
+        close(h);
+      catch e
+        disp(e);
+      end
+      
+      obj.stimCache{end + 1, 1} = stimulus.name;
+      obj.stimCache{end, 2} = stim;
     end
-
-    methods(Access = private)
-
-        %
-        % Load/cache stimulus
-        %
-        function stim = loadStimulus(obj, stimulus)
-            index = find(strcmp({obj.stimCache{:, 1}}, stimulus.name));
-
-            if ~isempty(index)
-                stim = obj.stimCache{index(1), 2};
-                return;
-            end
-                       
-            [~, filename, ~] = fileparts(stimulus.name);
-            stimInfo = obj.project.getInfoForStimulus(filename);
-
-            if ~isstruct(stimInfo)
-              warning('Invalid stimulus specified');
-              stim = [];
-              return;
-            end;
-            
-            stimFile = fullfile(stimInfo.resourcepath, stimInfo.filename);
-
-            h = waitbar(0, 'Opening stimulus...');
-
-            try
-                stim = VideoROIStimulus();
-                stim.openStimulus(stimFile);
-                close(h);
-            catch e
-                disp(e);
-            end                                    
-            
-            obj.stimCache{end + 1, 1} = stimulus.name;
-            obj.stimCache{end, 2} = stim;
-        end
-
-
-        function clearCache(obj)       
-            obj.stimCache = cell(0, 2);
-        end
-
-
-        function onChangeTrial(obj, ~, trialId)
-            obj.currentTrial = trialId;
-            obj.view.setCurrentTrial(obj.currentTrial);
-            [samples, columns] = obj.dataset.getAnnotationsForTrial(obj.currentTrial, 'pixels');
-
-            col_time = find(strcmp(columns, 'Time'));
-            col_x = find(strcmp(columns, 'R POR X [px]'));
-            col_y = find(strcmp(columns, 'R POR Y [px]'));
-
-            obj.view.updateTrace( ...
-                samples(:, col_time), ...
-                samples(:, [col_x col_y]));
-
-            obj.view.setTotalTime(diff(samples([1 end], col_time)));
-            obj.clearCache();
-        end
-
-
-        function onChangeTime(obj, ~, time)            
-            stimuli = obj.dataset.getStimuliAtTime(obj.currentTrial, time);           
-
-            % Append data for each stimulus
-            for i = 1:numel(stimuli)
-                stimulus = obj.loadStimulus(stimuli(i));                
-                
-                if ~isempty(stimulus)
-                  I = stimulus.readFrame(stimuli(i).frame);
-                else
-                  I = zeros(stimuli.position(3), stimuli.position(4), 3);
-                  I(1, :, 1) = 1;
-                  I(:, 1, 1) = 1;
-                  I(stimuli.position(3), :, 1) = 1;
-                  I(:, stimuli.position(4), 1) = 1;
-                end
-                
-                stimuli(i).data = I;
-            end
-            
-            % Ask view to update scene
-            obj.view.updateScreen(stimuli);
-        end
+    
+    
+    function clearCache(obj)
+      obj.stimCache = cell(0, 2);
     end
+    
+    
+    function onChangeTrial(obj, ~, trialId)
+      obj.currentTrial = trialId;
+      obj.view.setCurrentTrial(obj.currentTrial);
+      [samples, columns] = obj.dataset.getAnnotationsForTrial(obj.currentTrial, 'pixels');
+      
+      col_time = find(strcmp(columns, 'Time'));
+      col_x = find(strcmp(columns, 'R POR X [px]'));
+      col_y = find(strcmp(columns, 'R POR Y [px]'));
+      
+      obj.view.updateTrace( ...
+        samples(:, col_time), ...
+        samples(:, [col_x col_y]));
+      
+      obj.view.setTotalTime(diff(samples([1 end], col_time)));
+      obj.clearCache();
+    end
+    
+    
+    function onChangeTime(obj, ~, time)
+      stimuli = obj.dataset.getStimuliAtTime(obj.currentTrial, time);
+      
+      % Append data for each stimulus
+      for i = 1:numel(stimuli)
+        stimulus = obj.loadStimulus(stimuli(i));
+        
+        if ~isempty(stimulus)
+          I = stimulus.readFrame(stimuli(i).frame);
+        else
+          I = zeros(stimuli.position(3), stimuli.position(4), 3);
+          I(1, :, 1) = 1;
+          I(:, 1, 1) = 1;
+          I(stimuli.position(3), :, 1) = 1;
+          I(:, stimuli.position(4), 1) = 1;
+        end
+        
+        stimuli(i).data = I;
+      end
+      
+      % Ask view to update scene
+      obj.view.updateScreen(stimuli);
+    end
+  end
 end
--- src/videoroi/VideoROIStimulus.m
+++ src/videoroi/VideoROIStimulus.m
@@ -1,137 +1,137 @@
 classdef VideoROIStimulus < handle
-%
-% Represents a visual stimulus, which can be either a still image or
-% a video. This class presents still images as 1-frame videos.
-%
+  %
+  % Represents a visual stimulus, which can be either a still image or
+  % a video. This class presents still images as 1-frame videos.
+  %
+  
+  properties(Access = private)
+    type = 'none';
     
-    properties(Access = private)
-        type = 'none';
-
-        videoReader;
-        imageBuffer;
-
-        numFrames = 0;
-
-        frameWidth = 0;
-        frameHeight = 0;
-    end;
-
+    videoReader;
+    imageBuffer;
     
-    methods(Access = private)
-        % Opens a video (motion image) stimulus
-        function openVideo(obj, videoFile)
-            try
-                obj.videoReader = VideoReaderMM(videoFile);
-                obj.type = 'video';
-            catch e
-                message = [e.message 10 10 'Filename = ' videoFile];
-                error(message);
-            end
-            
-            obj.numFrames = obj.videoReader.NumberOfFrames;
-            
-            if(isempty(obj.numFrames))
-                fprintf('Stimulus: Scanning file to determine amount of frames...\n');
-                obj.videoReader.read(inf);
-                obj.numFrames = obj.videoReader.NumberOfFrames;                
-            end
-            
-            fprintf('Stimulus: Opened video with %d frames.\n', obj.numFrames);
-            
-            I = obj.readFrame(1);
-            obj.frameWidth = size(I, 2);
-            obj.frameHeight = size(I, 1);            
-        end
-
-        
-        % Opens an still-image stimulus
-        function openImage(obj, imageFile)
-            try
-                obj.imageBuffer = imread(imageFile);
-                obj.type = 'image';
-            catch e
-                error('The image file specified could not be found.');
-            end
-            
-            obj.numFrames = 1;
-            
-            size(obj.imageBuffer);
-            
-            obj.frameWidth = size(obj.imageBuffer, 2);
-            obj.frameHeight = size(obj.imageBuffer, 1);              
-        end
+    numFrames = 0;
+    
+    frameWidth = 0;
+    frameHeight = 0;
+  end;
+  
+  
+  methods(Access = private)
+    % Opens a video (motion image) stimulus
+    function openVideo(obj, videoFile)
+      try
+        obj.videoReader = VideoReaderMM(videoFile);
+        obj.type = 'video';
+      catch e
+        message = [e.message 10 10 'Filename = ' videoFile];
+        error(message);
+      end
+      
+      obj.numFrames = obj.videoReader.NumberOfFrames;
+      
+      if(isempty(obj.numFrames))
+        fprintf('Stimulus: Scanning file to determine amount of frames...\n');
+        obj.videoReader.read(inf);
+        obj.numFrames = obj.videoReader.NumberOfFrames;
+      end
+      
+      fprintf('Stimulus: Opened video with %d frames.\n', obj.numFrames);
+      
+      I = obj.readFrame(1);
+      obj.frameWidth = size(I, 2);
+      obj.frameHeight = size(I, 1);
     end
-
-
-    methods
-        function obj = VideoROIStimulus()
-            obj.type = 'none';
-            obj.numFrames = 0;
-            obj.frameWidth = 0;
-            obj.frameHeight = 0;            
-        end
-
-        
-        % Opens a stimulus, determining file-type by extension
-        function openStimulus(obj, stimulusFile)
-            videoExt = {'.wmv', '.avi', '.mpg', '.mpeg'};
-            imageExt = {'.jpeg', '.jpg', '.png', '.bmp', '.gif'};
-            
-            [path, name, ext] = fileparts(stimulusFile);
-            
-            ext = lower(ext);
-            
-            if(any(strcmp(ext, videoExt)))
-                obj.openVideo(stimulusFile);
-            end
-                
-            if(any(strcmp(ext, imageExt)))
-                obj.openImage(stimulusFile);
-            end
-        end
-        
-        
-        % Query number of frames
-        function numFrames = getNumberOfFrames(obj)
-            numFrames = obj.numFrames;
-        end;
-        
-        
-        % Query frame rate
-        function frameRate = getFrameRate(obj)
-            if(strcmp(obj.type, 'video') && isobject(obj.videoReader))
-                frameRate = obj.videoReader.FrameRate;
-            else
-                frameRate = 30;
-            end
-        end
-        
-        % Query frame width
-        function frameWidth = getFrameWidth(obj)
-            frameWidth = obj.frameWidth;
-        end
-
-        
-        % Query frame height
-        function frameHeight = getFrameHeight(obj)
-            frameHeight = obj.frameHeight;
-        end
-        
-        
-        % Retrieves a single frame
-        function I = readFrame(obj, index)
-            if strcmp(obj.type, 'video')
-                try
-                    I = obj.videoReader.read(index);
-                catch
-                    I = obj.videoReader.read(index);
-                end;
-            end;
-            
-            if strcmp(obj.type, 'image')
-                I = obj.imageBuffer;
-            end
-        end
+    
+    
+    % Opens an still-image stimulus
+    function openImage(obj, imageFile)
+      try
+        obj.imageBuffer = imread(imageFile);
+        obj.type = 'image';
+      catch e
+        error('The image file specified could not be found.');
+      end
+      
+      obj.numFrames = 1;
+      
+      size(obj.imageBuffer);
+      
+      obj.frameWidth = size(obj.imageBuffer, 2);
+      obj.frameHeight = size(obj.imageBuffer, 1);
+    end
+  end
+  
+  
+  methods
+    function obj = VideoROIStimulus()
+      obj.type = 'none';
+      obj.numFrames = 0;
+      obj.frameWidth = 0;
+      obj.frameHeight = 0;
+    end
+    
+    
+    % Opens a stimulus, determining file-type by extension
+    function openStimulus(obj, stimulusFile)
+      videoExt = {'.wmv', '.avi', '.mpg', '.mpeg'};
+      imageExt = {'.jpeg', '.jpg', '.png', '.bmp', '.gif'};
+      
+      [path, name, ext] = fileparts(stimulusFile);
+      
+      ext = lower(ext);
+      
+      if(any(strcmp(ext, videoExt)))
+        obj.openVideo(stimulusFile);
+      end
+      
+      if(any(strcmp(ext, imageExt)))
+        obj.openImage(stimulusFile);
+      end
+    end
+    
+    
+    % Query number of frames
+    function numFrames = getNumberOfFrames(obj)
+      numFrames = obj.numFrames;
     end;
     
+    
+    % Query frame rate
+    function frameRate = getFrameRate(obj)
+      if(strcmp(obj.type, 'video') && isobject(obj.videoReader))
+        frameRate = obj.videoReader.FrameRate;
+      else
+        frameRate = 30;
+      end
+    end
+    
+    % Query frame width
+    function frameWidth = getFrameWidth(obj)
+      frameWidth = obj.frameWidth;
+    end
+    
+    
+    % Query frame height
+    function frameHeight = getFrameHeight(obj)
+      frameHeight = obj.frameHeight;
+    end
+    
+    
+    % Retrieves a single frame
+    function I = readFrame(obj, index)
+      if strcmp(obj.type, 'video')
+        try
+          I = obj.videoReader.read(index);
+        catch
+          I = obj.videoReader.read(index);
+        end;
+      end;
+      
+      if strcmp(obj.type, 'image')
+        I = obj.imageBuffer;
+      end
+    end
+  end;
+  
 end
