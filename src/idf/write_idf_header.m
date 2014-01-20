dae9bc8512829bab4333ff1f0aa7f750bbbd40b4
Ivar Clemens <post@ivarclemens.nl>
2014-01-20 19:28:25 +0100

    Moved header writing code into separate function.

---
+++ src/idf/write_idf_header.m
@@ -0,0 +1,40 @@
+function write_idf_header(cfg, fid)
+% WRITE_HEADER  Writes the file header.
+
+  if(~isfield(cfg, 'columns'))
+      cfg.columns = {'Time', 'Type', 'Trial', ...
+          'R Raw X [px]', 'R Raw Y [px]', 'R Dia X [px]', 'R Dia Y [px]', ...
+          'R CR1 X [px]', 'R CR1 Y [px]', 'R POR X [px]', 'R POR Y [px]'};
+  end;
+
+  if(~isfield(cfg, 'sampleRate')), cfg.sampleRate = 500; end;
+  if(~isfield(cfg, 'sampleCount')), cfg.sampleCount = 1000; end;
+  if(~isfield(cfg, 'screenResolution')), cfg.screenResolution = [1024 768]; end;
+  if(~isfield(cfg, 'experiment')), cfg.experiment = 4; end;
+  if(~isfield(cfg, 'participant')), cfg.participant = 102; end;
+
+  fprintf(fid, '## [iView]\n');
+  fprintf(fid, '## Converted from: generated.idf\n');
+  fprintf(fid, '## Date: %s\n', datestr(now, 'dd.mm.yyyy hh:MM:ss'));
+  fprintf(fid, '## Version: IDF Converter 3.0.8\n');
+  fprintf(fid, '## Sample Rate: %d\n', cfg.sampleRate);
+
+  fprintf(fid, '## [Run]\n');
+  fprintf(fid, '## Subject:	Experiment%d_s%d\n', cfg.experiment, cfg.participant);
+  fprintf(fid, '## Description:\n');
+  fprintf(fid, '## [Calibration]\n');
+  fprintf(fid, '## Calibration Type:	13-point\n');
+  fprintf(fid, '## Calibration Area:	%d %d\n', cfg.screenResolution(1), cfg.screenResolution(2));
+  fprintf(fid, '## [Geometry]\n');
+  fprintf(fid, '## Stimulus Dimension [mm]:	340	270\n');
+  fprintf(fid, '## Head Distance [mm]:	500\n');
+  fprintf(fid, '## [Hardware Setup]\n');
+  fprintf(fid, '## [Presentation]\n');
+  fprintf(fid, '## Number of Samples:	%d\n', cfg.sampleCount);
+  fprintf(fid, '## Reversed:	none\n');
+  fprintf(fid, '## Format:	RIGHT, RAW, DIAMETER, CR, POR, MSG\n');
+  fprintf(fid, '## \n');
+  fprintf(fid, '\n');
+
+  fprintf(fid, '%s\n', strjoin(cfg.columns, '\t'));
+
--- test/DatasetGenerator.m
+++ test/DatasetGenerator.m
@@ -1,46 +1,5 @@
 function DatasetGenerator(cfg)
 
-    function write_header(cfg)
-    % WRITE_HEADER  Writes the file header.
-    
-        fid = cfg.datasetFile;
-        
-        if(~isfield(cfg, 'columns'))
-            cfg.columns = {'Time', 'Type', 'Trial', ...
-                'R Raw X [px]', 'R Raw Y [px]', 'R Dia X [px]', 'R Dia Y [px]', ...
-                'R CR1 X [px]', 'R CR1 Y [px]', 'R POR X [px]', 'R POR Y [px]'};
-        end;
-        
-        if(~isfield(cfg, 'sampleRate')), cfg.sampleRate = 500; end;
-        if(~isfield(cfg, 'sampleCount')), cfg.sampleCount = 1000; end;
-        if(~isfield(cfg, 'screenResolution')), cfg.screenResolution = [1024 768]; end;
-        
-        fprintf(fid, '## [iView]\n');
-        fprintf(fid, '## Converted from: generated.idf\n');
-        fprintf(fid, '## Date: %s\n', datestr(now, 'dd.mm.yyyy hh:MM:ss'));
-        fprintf(fid, '## Version: IDF Converter 3.0.8\n');
-        fprintf(fid, '## Sample Rate: %d\n', cfg.sampleRate);
-        
-        fprintf(fid, '## [Run]\n');
-        fprintf(fid, '## Subject:	Experiment4_s102\n');
-        fprintf(fid, '## Description:\n');
-        fprintf(fid, '## [Calibration]\n');
-        fprintf(fid, '## Calibration Type:	13-point\n');
-        fprintf(fid, '## Calibration Area:	%d %d\n', cfg.screenResolution(1), cfg.screenResolution(2));
-        fprintf(fid, '## [Geometry]\n');
-        fprintf(fid, '## Stimulus Dimension [mm]:	340	270\n');
-        fprintf(fid, '## Head Distance [mm]:	500\n');
-        fprintf(fid, '## [Hardware Setup]\n');
-        fprintf(fid, '## [Presentation]\n');
-        fprintf(fid, '## Number of Samples:	%d\n', cfg.sampleCount);
-        fprintf(fid, '## Reversed:	none\n');
-        fprintf(fid, '## Format:	RIGHT, RAW, DIAMETER, CR, POR, MSG\n');
-        fprintf(fid, '## \n');
-        fprintf(fid, '\n');
-        
-        fprintf(fid, '%s\n', strjoin(cfg.columns, '\t'));
-    end  
-
     function time = generate_samples(scfg)
     % GENERATE_SAMPLES  Writes all samples for a given trial 
     % to the datafile.
@@ -115,7 +74,7 @@ function DatasetGenerator(cfg)
         cfg.startTime = 0;
         time = cfg.startTime;
 
-        write_header(cfg);
+        write_idf_header(cfg, cfg.datasetFile);
         
         for i = 1:length(cfg.stimuli)
             stimulus = VideoROIStimulus();
