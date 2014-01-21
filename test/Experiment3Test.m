9a4c6432f9a7437f34a59b28e365e34df47d2234
Ivar Clemens <post@ivarclemens.nl>
2014-01-21 10:06:08 +0100

    Added some scenarios to the 1/3 test script.

--- test/Experiment3Test.m
+++ test/Experiment3Test.m
@@ -1,5 +1,31 @@
 function Experiment3Test(cfg)
   
+  % Stimulus dimensions:
+  %  L 279 W 465
+  %  T 34  H 700
+  
+  % ROI Top (35 to 300) and ROI Bottom (400 to 700)
+  
+  % ms, x, y
+  scenarios = { ...
+    ... %  - Constant fixation for entire duration (inside image)
+    {15000, 350, 100}, ...  % 1. Fixation inside ROI
+    {15000, 200, 600}, ...  % 2. Fixation outside image
+    {15000, 350, 350}, ...  % 3. Fixation outside ROI
+    ... %  - Fixation jumps to image 1000ms after image onset and away 1000ms before image offset.
+    {2000, 350, 100; 7000, 350, 100; 1500, 350, 100}, ...
+    {2000, 200, 600; 7000, 200, 600; 1500, 200, 600}, ...
+    {2000, 350, 350; 7000, 350, 350; 1500, 350, 350}, ...
+    ... % - Fixation jumps to image 250ms before image onset and away at image offset.
+    {750, 350, 100; 9250, 100, 350; 250, 350, 100}, ...
+    {750, 350, 600; 9250, 600, 350; 250, 350, 600}, ...
+    {750, 350, 350; 9250, 350, 350; 250, 350, 350}, ...
+    ... %  - Fixation jumps every 400ms, Outside image -> ROI 1 -> Image -> ROI 2 -> ROI 3 -> Outside
+    {
+    
+    }
+    };
+  
     function time = generate_samples(scfg)
     % GENERATE_SAMPLES  Writes all samples for a given trial 
     % to the datafile.
@@ -7,32 +33,16 @@ function Experiment3Test(cfg)
     % All trials are structured similarly:
     %  [PRE-FIXATION, FIXATION, PICTURE, POST-PICTURE] = [500, 500, 9000, 500]
     %
-    % Scenarios:
-    %  - Constant fixation for entire duration (inside image)
-    %   1. ROI Full image
-    %   2. ROI Outside fixation
-    %   3. ROI Around fixation
-    %
-    %  - Fixation jumps to image 1000ms after image onset and away 1000ms before image offset.
-    %   4. ROI Full image
-    %   5. ROI Outside fixation
-    %   6. ROI Around fixation
-    %  - Fixation jumps to image 1000ms before image onset and away at image offset.
-    %   7. ROI Full image
-    %   8. ROI Outside fixation
-    %   9. ROI Around fixation
-    %
-    %  - Fixation jumps every 400ms, Outside image -> ROI 1 -> Image -> ROI 2 -> ROI 3 -> Outside
-    %   10. Three regions of interest
-    %
 
+        if ~isfield(scfg, 'prefixDuration'), scfg.prefixDuration = 500 * 1000; end
         if ~isfield(scfg, 'fixationDuration'), scfg.fixationDuration = 500 * 1000; end
         if ~isfield(scfg, 'trialDuration'), scfg.trialDuration = 9000 * 1000; end
+        if ~isfield(scfg, 'postpctDuration'), scfg.postpctduration = 500 * 1000; end
         
         time = scfg.startTime;
         fid = scfg.datasetFile;
         
-        fixMsgTime = time + 500 * 1000;
+        fixMsgTime = time + scfg.prefixDuration;
         pctMsgTime = fixMsgTime + scfg.fixationDuration;
         eopMsgTime = pctMsgTime + scfg.fixationDuration + scfg.trialDuration;        
         
@@ -40,7 +50,7 @@ function Experiment3Test(cfg)
         pctMsgFlag = 0;
         eopMsgFlag = 0;        
         
-        numberOfSamples = (500 * 1000 + eopMsgTime - time) / 2;
+        numberOfSamples = (scfg.postpctDuration + eopMsgTime - time) / 2;
         
         fprintf(fid, '%d\tMSG\t%d\t# Message: New trial: TrialNr = %d\n', time, scfg.trial - 1, scfg.trialNr);
 
@@ -92,19 +102,17 @@ function Experiment3Test(cfg)
         write_idf_header(cfg, cfg.datasetFile);
         
         % Loop over trials (one per stimulus)
-        for i = 1:length(cfg.stimuli)
-            stimulus = VideoROIStimulus();
-            stimulus.openStimulus(cfg.stimuli{i});
-            
+        for i = 1:length(cfg.stimuli)            
             scfg = struct();
-            scfg.picturePosition = [279 34];
-                        
-            [~, name, ext] = fileparts(cfg.stimuli{i});
-            scfg.filename = [name ext];
+            scfg.datasetFile = cfg.datasetFile;
             scfg.startTime = time;
+
             scfg.trial = i;
             scfg.trialNr = i;
-            scfg.datasetFile = cfg.datasetFile;
+            
+            % Stimulus
+            scfg.filename = cfg.stimuli{i};
+            scfg.picturePosition = [279 34];
             
             time = generate_samples(scfg);            
         end                       
